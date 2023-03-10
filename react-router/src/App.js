2bb9fd83ebe1ce8ba4f7c963328e38f55cce91a4
Antonino Musmeci <nino.musmeci@gmail.com>
2023-03-10 19:24:30 +0100

    react-router/ using createRoutes v6.4

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,26 +1,45 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
+//Layout
+import RootLayout from "./layout/RootLayout";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>"react-router"</h1>
-        <NavLink to="/"> Home </NavLink>
-        <NavLink to ="about">About </NavLink>
-      </nav>
-    </header>
-      <main>
-        <h1> Hi </h1>
-        <Routes>
-          <Route index element={<Home/>} />
-          <Route path="about" element={<About/>} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+
+    // <BrowserRouter>
+    // <header>
+    //   <nav>
+    //     <h1>"react-router"</h1>
+    //     <NavLink to="/"> Home </NavLink>
+    //     <NavLink to ="about">About </NavLink>
+    //   </nav>
+    // </header>
+    //   <main>
+    //     <h1> Hi </h1>
+    //     <Routes>
+    //       <Route index element={<Home/>} />
+    //       <Route path="about" element={<About/>} />
+    //     </Routes>
+    //   </main>
+    // </BrowserRouter>
+
+    <RouterProvider router = {router} />
   );
 }
 
