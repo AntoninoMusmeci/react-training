fab2eb5ddcaa36297957e62cbafa8537a03bc212
Antonino Musmeci <nino.musmeci@gmail.com>
2023-03-11 15:10:29 +0100

    react-router/ use Form component and actions

--- react-router/src/pages/help/Contact.js
+++ react-router/src/pages/help/Contact.js
@@ -1,10 +1,11 @@
-import React from "react";
+import {Form, redirect, useActionData} from "react-router-dom"
 
 const Contact = () => {
+  const data = useActionData()
   return (
     <div className="contact">
       <h3>Contact Us</h3>
-      <form>
+      <Form method ="post" action="/help/contact">
         <label >
           <span>Your email:</span>
           <input type="email" name="email" required />
@@ -14,9 +15,24 @@ const Contact = () => {
           <textarea name="message" required ></textarea>
         </label>
         <button>Submit</button>
-      </form>
+      </Form>
+      {data && data.message && <p>{data.message}</p>}
     </div>
   );
 };
 
 export default Contact;
+
+export const contactAction = async ({request}) => {
+  const data = await request.formData()
+  const submission = {
+    email: data.get("email"),
+    message: data.get("message")
+  }
+  if(submission.message.length  < 10){
+    return {message: "error!!!"}
+  }
+  
+  console.log(submission)
+  return redirect("/")
+}
